2583da090442201cd7660d2fa6a0cda5f99a8f6a
Tor Vigesdal <tor@netsecurity.no>
2019-10-21 10:43:16 +0200

    Final product using CSV file

--- Publish-PPTX-Speech.ps1
+++ Publish-PPTX-Speech.ps1
@@ -1,75 +1,49 @@
 <#
-   .SYNOPSIS
- 		This script automates the running of a slide show, 
-        including text-to-speech.
+    .SYNOPSIS
+    This script automates the running of a slide show, 
+    including text-to-speech and (pending) generation of .SRT subtitle files.
         
-	.DESCRIPTION
-		Update the Publish-PPTX.Speech.XLSX worksheet, 
-        or update the .CSV file if you do not have Excel available.
-  #========================================================================
+    .DESCRIPTION
+	Update the Publish-PPTX.Speech.XLSX worksheet to generate .CSV file.
+    Alternatively update the .CSV file directly if you do not have Excel available.
+  
+    .LINK
+    https://github.com/dotBATmanNO/PSPublish-PPTX-Speech/
 #>
+
+[CmdletBinding(PositionalBinding=$false)]
+
+ param (
+    # The full path of PPTX source file.
+    # Note: The corresponding .CSV file must exist as well.
+    [Parameter(Position=0)][string]$Path,
+    # By default the script will generate Slidexxx.wav files in Record Folder
+    # Use -SaveFile 0 if you want to run the slideshow on read-only media.
+    $SaveFile = $true,
+    # Only one gender is supported for Slidexxx.wav files.
+    # Pro-tip: Purchase a professional voice or enable Cortana for Speech-to-Text.
+    $SaveFileGender = "Female")
 
 Function Out-Speech 
 {
- <#
-
-  #========================================================================
-  # Created on:    2/18/2014 2:42 AM
-  # Created by:    Guido Oliveira
-  # Function name: Out-Speech
-  #========================================================================
-	.SYNOPSIS
-		This is a Text to Speech Function made in powershell.
-
-	.DESCRIPTION
-		This is a Text to Speech Function made in powershell.
-
-	.PARAMETER  Message
-		Type in the message you want to hear.
-
-	.PARAMETER  Gender
-		The description of a the ParameterB parameter.
-
-	.EXAMPLE
-		PS C:\> Out-Speech -Message "Testing the function" -Gender 'Female'
-		
-	.EXAMPLE
-		PS C:\> "Testing the function 1","Testing the function 2","Testing the function 3","Testing the function 4","Testing the function 5","Testing the function 6" | Foreach-Object { Out-Speech -Message $_ }
-	
-	.EXAMPLE
-		PS C:\> "Testing the PS-Pipeline" | Out-Speech
-
-	.INPUTS
-		System.String
-
-	#>
+    # Based on Out-Speech created by Guido Oliveira
+   	
 	[CmdletBinding()]
 	param(
-	  [Parameter(Mandatory=$true)]
-	  [String[]]
-	  $Message="Test Message.",
-	  [Parameter(Mandatory=$true)]
-      [String[]]
-	  $Gender="Female")
+	  [String[]]$Message="Test Message.",
+	  [String[]]$Gender="Female")
 	begin 
     {
-	  try
-      {
-	   Add-Type -Assembly System.Speech -ErrorAction Stop
-	  }
-	  catch 
-      {
-       Write-Error -Message "Error loading the required assemblies"
-      }
+	  try   { Add-Type -Assembly System.Speech -ErrorAction Stop }
+	  catch { Write-Error -Message "Error loading the required assemblies" }
 	}
 	process
     {
+
      $voice = New-Object -TypeName 'System.Speech.Synthesis.SpeechSynthesizer' -ErrorAction Stop
             
-     #Write-Verbose "Selecting a $Gender voice"
      $voice.SelectVoiceByHints($Gender)
             			
-     #Write-Verbose -Message "Start Speaking"
      $voice.Speak($message) | Out-Null
 				
 	}
@@ -79,50 +53,78 @@ Function Out-Speech
 	}
 }
 
-$ScriptPath = Split-Path -parent $PSCommandPath
-$RecordPath = Join-Path -Path $ScriptPath -ChildPath "Record"
-If ( (Test-Path $RecordPath) -eq $False ) 
+Function fnSaveWAVFile
 {
- New-Item -Path $ScriptPath -Name "Record" -ItemType "Directory"
-}
+    # Based on Out-Speech created by Guido Oliveira
+   	
+    [CmdletBinding()]
+	param(
+	  [String[]]$Message="Test Message.",
+	  [String]$WAVFileName)
+    
+    try   { Add-Type -Assembly System.Speech -ErrorAction Stop }
+    catch { Write-Error -Message "Error loading the required assemblies" }
 
-If ( (Test-Path $PPTXFile) -eq $False ) 
+    $voicesave = New-Object -TypeName 'System.Speech.Synthesis.SpeechSynthesizer' -ErrorAction Stop
+    
+    $voicesave.SelectVoiceByHints($SaveFileGender)
+    $Voicesave.SetOutputToWaveFile($WAVFileOut)
+    ForEach ($strmsg in $Message) { $Voicesave.Speak($strmsg) }
+    $voicesave.Dispose()
+
+} # End function fnSaveWAVFile
+
+# Start of main script. Check input first.
+If ( $Path -eq "" -or (Test-Path $Path) -eq $False ) 
 {
- Write-Host "The PPTX file $($PPTXFile) was not found!"
+  Write-Host "The PowerPoint file '$($Path)' was not found!"
 }
 else
 {
 
-    $ScriptPath = Split-Path -parent $PSCommandPath
+    $PPTXPath = Split-Path -parent $Path                                  # Retrieve PATH of PPTX file
+    $PPTXFileName = [System.IO.Path]::GetFileNameWithoutExtension($Path)  # Retrieve filename of PPTX file (no extension)
+    $SlideCSVFile = "$($PPTXPath)\$($PPTXFileName).CSV"                   # Build variable holding name of CSV file
+    If ( (Test-Path $SlideCSVFile) -eq $False ) 
+    {
+      Write-Host "The PowerPoint file needs to be supported by a CSV file named '$($SlideCSVFile)!"
+      Write-Host "Generate your .CSV file using the Publish-PPTX-Speech.XLSX file."
+      Write-Host ".. or copy and edit Publish-PPTX-Speech.csv if you do not have Excel."
+      Break
+    }
     
-    $SaveFile = $true
-    $SaveFileGender = "Female"
-
+    If ($SaveFile) # User wants to save Slidexxx.wav files
+    {
+        $RecordPath = Join-Path -Path $PPTXPath -ChildPath "Record"           # Check for and prepare folder for .WAV files
+        If ( (Test-Path $RecordPath) -eq $False ) 
+        {
+            New-Item -Path $PPTXPath -Name "Record" -ItemType "Directory" | Out-Null
+        }
+        
+    }
+    
     $ppAdvanceOnClick = 0
     $ppSlideShowRehearseNewTimings = 3
     $ppSlideShowPointerAlwaysHidden = 3
 
-    $ppShowTypeKiosk = 3
-    $ppSlideShowDone = 5
+    # $ppShowTypeKiosk = 3
+    # $ppSlideShowDone = 5
 
     $objPPT = New-Object -ComObject "PowerPoint.Application"
     $objPPT.Visible = 1
 
-    $objPresentation = $objPPT.Presentations.Open($PPTXFile, $true,$false,$false)
+    Try
+    {
+        # Using SaveFile parameter to decide if new Slide Advance timings shall be saved.
+        $objPresentation = $objPPT.Presentations.Open($Path, !$SaveFile,$false,$false)
+    }
+    Catch 
+    {
+        write-host "File '$($path)' failed to open in PowerPoint."
+        Break   
+    }
 
-    # Disable automatic transition, we want to use transition in NOTES section of slide
-    #
-    # Slidexxx,TransitionDuration,ClickForNext (Boolean True/False),Gender of voice,Text to say
-    # Example:
-    # Slide001,7,True,"Male","Risk Management.  A real-world scenario for Car Owners."
-    # |- Slide 001   = Must match the slide it is on, this is a
-    #                  key to process the line as text-to-speech
-    # |- 1           = Number of seconds to pause after text-to-speech completes
-    # |- True        = After the transition has completed, 
-    #                  automatically click the next animation
-    # |- Male        = Use the male text-to-speech engine
-    # |- Text To Say = Self-explanatory... Note Language! :)
-
+    # Disable automatic transition, we want to use transitions from PPTXFileName.csv
     $objPresentation.SlideShowSettings.AdvanceMode = $ppAdvanceOnClick
 
     # $objPresentation.SlideShowSettings.ShowType = $ppShowTypeKiosk
@@ -132,107 +134,95 @@ else
 
     $objPresentation.SlideShowSettings.EndingSlide = $objPresentation.Slides.Count
 
-
     $objSlideShow = $objPresentation.SlideShowSettings.Run().View
     Start-Sleep -Seconds 2
+
     $objSlideShow.PointerType = $ppSlideShowPointerAlwaysHidden
 
-    $arrSteps = Input-Csv -Path $ScriptPath\ -header "SlideNumber", "Duration", "Click", "Gender", "Say"
- 
+    #TryCSVBlock
+    Try 
+    {
+        $arrSteps = Import-Csv -Path $SlideCSVfile -header "SlideNumber", "Duration", "Click", "Gender", "Say"
+    }
+    Catch
+    {
+        Write-Host "Unable to read slide transitions from CSV file '$($SlideCSVFile)'."
+        Break   
+    } # End TryCSVBlock
+
     # Enumerate and run through all slides
     For ($CurSlide=1; $CurSlide -le $objPresentation.Slides.Count; $CurSlide++)
     {
- 
-     $intSlideDuration = 0
-     # Build search pattern; 001 to 999 supported, any more than 999 slides would be #unthinkable
-     $strCurSlide = $CurSlide.ToString().PadLeft(3, "0")
-     $strPattern = "Slide$StrCurSlide"
- 
-     # Connect to the Slide Comments text box
-     $SlideNotes = $objPresentation.Slides($CurSlide).NotesPage.Shapes.Placeholders(2).TextFrame
- 
-     # If there are Slide Comments, let's find those that need to be converted to Speech
-     If ($SlideNotes.HasText)
-     {
-      $SlideCurClick = 0
-      $NotesLine = 1
-      $strSlideMessage = ""
-  
-      While ($NotesLine -le $SlideNotes.TextRange.Paragraphs().Count)
-      {
-       $NotesCurLine = $SlideNotes.TextRange.Paragraphs($NotesLine,1).Text
-       Write-Verbose $NotesCurLine
-       If ($NotesCurLine.Length -gt 8)
-       {
-        If ($NotesCurLine.SubString(0,8) -eq $strPattern)
-        {
-    
-            $Transition = ConvertFrom-Csv -InputObject $NotesCurLine -header "SlideNumber", "Duration", "Click", "Gender", "Say"
-    
-            $strMessage = $Transition.Say
-            $strGender = $Transition.Gender
-            Write-Verbose "$strMessage by $strGender"
-    
-            # Build a variable holding all text-to-speech for this slide
-            $strSlideMessage += $strMessage
         
-            $speaktime = measure-command { Out-Speech -Message $strMessage -Gender $strGender }
-            Write-Verbose $speaktime
-            $timetosleep = ($Transition.Duration - $speaktime.Seconds)
-            $intSlideDuration = $intSlideDuration + $speaktime + $timetosleep
+        $intSlideDuration = 0
+        # Build search pattern; 001 to 999 supported, any more than 999 slides would be #unthinkable
+        $strCurSlide = $CurSlide.ToString().PadLeft(3, "0")
+        $strPattern = "Slide$StrCurSlide"
+        
+        $SlideNotes = $arrSteps.Where{ $_.SlideNumber -eq $strPattern }
+        If ($SlideNotes.Count -gt 0) # Handle slides that do not have text-to-speech.
+        {
+            $SlideCurClick = 0
+            
+            $strSlideMessage = ""
 
-            If ($timetosleep -gt 0 -and $Transition.Duration -gt 0) 
+            ForEach ($Transition in $SlideNotes)
             {
-             Write-Verbose "Sleeping $timetosleep"
-             Start-sleep -Seconds $timetosleep
-            }
-            Else
-            {
-             If ($Transition.Duration -gt 0) {Write-Host "Transition $Transition takes too long to say ($speaktime seconds)!" -ForegroundColor Red }
-            }
-            If ($Transition.Click -like "True" -and $objSlideShow.GetClickCount() -ge $SlideCurClick)
-            {
-             $SlideCurClick++
-             # Click for next animation / slides
-             $objSlideShow.GotoClick($SlideCurClick)
-             $objSlideShow.PointerType = $ppSlideShowPointerAlwaysHidden
-            }
-        } 
-       }
-       $NotesLine++
-      }
- 
-      If ($SaveFile)
-      {
-       # Build
-       try
-       {
-        Add-Type -Assembly System.Speech -ErrorAction Stop
-        $voicesave = New-Object -TypeName 'System.Speech.Synthesis.SpeechSynthesizer' -ErrorAction Stop
-       }
-       catch 
-       {
-        Write-Error -Message "Error loading the requered assemblies"
-       }
+                
+                $strMessage = $Transition.Say
+                $strGender = $Transition.Gender
+                Write-Verbose "$strMessage by $strGender"
+                # Build a variable holding all text-to-speech for this slide
+                # This can be used to create one .wav file per slide
+                $strSlideMessage += $strMessage
 
-       $WAVFileOut = Join-Path -Path $ScriptPath -ChildPath "Record\Slide$($strCurSlide).wav"
-       $voicesave.SelectVoiceByHints($SaveFileGender)
-       $Voicesave.SetOutputToWaveFile($WAVFileOut)
-       ForEach ($strmsg in $strSlideMessage) { $Voicesave.Speak($strmsg)}
-       $voicesave.Dispose()
-      }
-  
-      If ($CurSlide -lt $objPresentation.Slides.Count)
-      { 
-       $objPresentation.SlideShowWindow.View.GotoSlide($CurSlide + 1)
-      }
- 
-      #Start-Sleep $objSlideShow.Slide.SlideshowTransition.AdvanceTime
-      Write-Host "Slide $CurSlide took $intSlideDuration seconds."
-     }
- 
-    }
+                $speaktime = Measure-Command { Out-Speech -Message $strMessage -Gender $strGender }
+                Write-Verbose $speaktime
+                $timetosleep = $Transition.Duration
+                $intSlideDuration = $intSlideDuration + [math]::Round($speaktime.TotalSeconds,1) + $timetosleep
 
+                If ($Transition.Duration -gt 0) 
+                {
+                    Write-Verbose "Sleeping $timetosleep"
+                    Start-sleep -Seconds $timetosleep
+                }
+                
+                If ($Transition.Click -like "True" -and $objSlideShow.GetClickCount() -ge $SlideCurClick)
+                {
+                    $SlideCurClick++
+                    # Click for next animation / slides
+                    $objSlideShow.GotoClick($SlideCurClick)
+                    # Setting PointerType forces screen refresh
+                    $objSlideShow.PointerType = $ppSlideShowPointerAlwaysHidden
+                }
+            } # All transitions for slide have been processed
+
+            If ($SaveFile) # User wants to save Slidexxx.wav files
+            {
+                $WAVFileOut = Join-Path -Path $RecordPath -ChildPath "Slide$($strCurSlide).wav"
+                fnSaveWAVFile $strSlideMessage -WAVFileName $WAVFileOut
+            }
+         
+        }
+        else
+        {
+            # Nothing to say, let's just show the slide for 5 seconds
+            $objSlideShow.PointerType = $ppSlideShowPointerAlwaysHidden
+            $intSlideDuration = 5.0
+            Start-Sleep -Seconds $intSlideDuration
+        }
+        
+        If ($SaveFile) { $objPresentation.Slides($CurSlide).SlideShowTransition.AdvanceTime = $intSlideDuration }
+         
+        If ($CurSlide -lt $objPresentation.Slides.Count)
+        { 
+            $objPresentation.SlideShowWindow.View.GotoSlide($CurSlide + 1)
+        }
+        Write-Host "Slide $CurSlide was shown for $intSlideDuration seconds."
+        
+    } # Reached the end of slide-show
+
+    If ($SaveFile) { $objPresentation.Save() }
     $objPresentation.Close()
     $objPPT.Quit()
-}
+} 
